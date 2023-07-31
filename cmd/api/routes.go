36d25848e337165e0ce27e5fa900666c55d34ee9
Owen Adirah <owenadira@gmail.com>
2023-07-31 22:53:28 +0300

    parse query params and return list of matching movies

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -17,6 +17,7 @@ func (app *application) routes() *httprouter.Router { // Initialize a new httpro
 
 	// CRUD operations endpoints
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
